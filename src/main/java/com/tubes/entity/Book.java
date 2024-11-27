8933a95b1220ff7a9aa199e4192d0c22b1feebe7
MkholidK09 <kholid.uuuy@gmail.com>
2024-11-27 20:23:42 +0700

    Fix folder structure (#3)
    Co-authored-by: mkholidkamali <kholid@premiro.com>

---
+++ src/main/java/com/tubes/entity/Book.java
@@ -0,0 +1,16 @@
+package com.tubes.entity;
+
+
+// @Entity
+// public class Book {
+//     private String id;
+//     private String author;
+//     private String genre;
+//     private String dateReleased;
+//     private String totalPage;
+//     private String description;
+//     private double rate;
+//     private String cover;
+
+//     public Book(){}
+// }
