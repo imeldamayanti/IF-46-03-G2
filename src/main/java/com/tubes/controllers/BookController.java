8b165d8c5aabba6caea20a9d5dc8f391b56a7d82
mkholidkamali <kholid@premiro.com>
2024-11-30 02:05:08 +0700

    BE to FE example

--- src/main/java/com/tubes/controllers/BookController.java
+++ src/main/java/com/tubes/controllers/BookController.java
@@ -4,17 +4,23 @@ import java.util.List;
 import com.tubes.service.BookService;
 import com.tubes.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
     @GetMapping("/books")
-    public List<Book> getBooks() {
-        return bookService.getAllBooks();
+    public String getBooks(Model model) {
+        List<Book> books = bookService.getAllBooks();
+
+        model.addAttribute("books", books);
+
+        // Look for a template in : src/main/resources/templates/...
+        return "booksExample";
     }
 }
