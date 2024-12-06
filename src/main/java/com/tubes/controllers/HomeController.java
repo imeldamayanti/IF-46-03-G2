d270712b9d3788fb51809eb91bcfea7b4400500f
Tsabita Bayu Kandi <bayutsabita@gmail.com>
2024-12-06 10:42:58 +0700

    edit sign in and sign up FE

--- src/main/java/com/tubes/controllers/HomeController.java
+++ src/main/java/com/tubes/controllers/HomeController.java
@@ -2,6 +2,8 @@ package com.tubes.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller 
@@ -32,6 +34,12 @@ public class HomeController {
     public String faq(){
         return "faq";
     }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+    
 
     // nanti logic atau BE nya bakal di sini untuk masing2 page (ini contoh home aja)
 }
