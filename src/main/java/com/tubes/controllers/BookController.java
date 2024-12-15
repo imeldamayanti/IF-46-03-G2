80d83060e36e3fd363b77b644be1b80e37aa13a0
imeldamayanti <imeldamayantisipit@gmail.com>
2024-12-15 21:11:51 +0700

    Book pagination

--- src/main/java/com/tubes/controllers/BookController.java
+++ src/main/java/com/tubes/controllers/BookController.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import com.tubes.service.BookService;
 import com.tubes.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
@@ -17,10 +19,17 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/books")
-    public String getBooks(Model model) {
-        List<Book> books = bookService.getAllBooks();
+    public String getBooks(
+        @RequestParam(value = "page", defaultValue = "1") int page,
+        @RequestParam(value = "size", defaultValue = "10") int size,
+        Model model
+    ) {
+        Page<Book> books = bookService.getAllBooks(page, size);
 
-        model.addAttribute("books", books);
+        model.addAttribute("books", books.getContent());
+        model.addAttribute("currentPage", books.getNumber() + 1);
+        model.addAttribute("totalPages", books.getTotalPages()); 
+        model.addAttribute("totalItems", books.getTotalElements());
 
         // Look for a template in : src/main/resources/templates/...
         return "booksExample";
--- src/main/java/com/tubes/service/BookService.java
+++ src/main/java/com/tubes/service/BookService.java
@@ -4,7 +4,12 @@ import java.util.List;
 import com.tubes.entity.Book;
 import com.tubes.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
 
 @Service
 public class BookService {
@@ -12,8 +17,14 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public Page<Book> getAllBooks(
+        @RequestParam(value = "page", defaultValue = "1") int page,
+        @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page-1, size);
+        Page<Book> booksPage = bookRepository.findAll(pageable);
+
+        return booksPage;
     }
 
     public List<Book> getBooksByGenre(String genre) {
